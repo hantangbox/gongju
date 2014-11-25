1800ab2a163329c73a8adfde40dd350b89cd98cf
ergou <654955321@qq.com>
2014-11-25 22:29:16 +0800

    更新1.0.3.4 （薇恩 ez 复仇之矛 奥巴马 纳尔）

---
+++ Marksman ADC合集（全英雄汉化） by Esk0r/Kogmaw.cs
@@ -0,0 +1,184 @@
+﻿#region
+
+using System;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Marksman
+{
+    internal class Kogmaw : Champion
+    {
+        public Spell E;
+        public Spell Q;
+        public Spell R;
+        public int UltimateBuffStacks = 0;
+        public Spell W;
+
+        public Kogmaw()
+        {
+            Utils.PrintMessage("KogMaw loaded 姹夊寲by浜岀嫍!QQ缇361630847.");
+
+            Q = new Spell(SpellSlot.Q, 1200f);
+            W = new Spell(SpellSlot.W, float.MaxValue);
+            E = new Spell(SpellSlot.E, 1360f);
+            R = new Spell(SpellSlot.R, float.MaxValue);
+
+            Q.SetSkillshot(0.25f, 70f, 1650f, true, SkillshotType.SkillshotLine);
+            E.SetSkillshot(0.25f, 120f, 1400f, false, SkillshotType.SkillshotLine);
+            R.SetSkillshot(1.2f, 120f, float.MaxValue, false, SkillshotType.SkillshotCircle);
+        }
+
+        public override void Drawing_OnDraw(EventArgs args)
+        {
+            Spell[] spellList = { Q, W, E, R };
+            foreach (var spell in spellList)
+            {
+                var menuItem = GetValue<Circle>("Draw" + spell.Slot);
+                if (menuItem.Active)
+                    Utility.DrawCircle(ObjectManager.Player.Position,
+                        spell.Slot == SpellSlot.W ? Orbwalking.GetRealAutoAttackRange(null) + 65 + W.Range : spell.Range,
+                        menuItem.Color);
+            }
+        }
+
+        public override void Game_OnGameUpdate(EventArgs args)
+        {
+            UltimateBuffStacks = GetUltimateBuffStacks();
+            W.Range = 110 + 20 * ObjectManager.Player.Spellbook.GetSpell(SpellSlot.W).Level;
+            R.Range = 900 + 300 * ObjectManager.Player.Spellbook.GetSpell(SpellSlot.R).Level;
+
+            if (R.IsReady() && GetValue<bool>("UseRM"))
+                foreach (
+                    var hero in
+                        ObjectManager.Get<Obj_AI_Hero>()
+                            .Where(
+                                hero => hero.IsValidTarget(R.Range) && R.GetDamage(hero) > hero.Health))
+                    R.Cast(hero, false, true);
+
+            if ((!ComboActive && !HarassActive) ||
+                (!Orbwalking.CanMove(100) &&
+                 !(ObjectManager.Player.BaseAbilityDamage + ObjectManager.Player.FlatMagicDamageMod > 100))) return;
+
+            var useQ = GetValue<bool>("UseQ" + (ComboActive ? "C" : "H"));
+            var useW = GetValue<bool>("UseW" + (ComboActive ? "C" : "H"));
+            var useE = GetValue<bool>("UseE" + (ComboActive ? "C" : "H"));
+            var useR = GetValue<bool>("UseR" + (ComboActive ? "C" : "H"));
+            var rLim = GetValue<Slider>("Rlim" + (ComboActive ? "C" : "H")).Value;
+
+            if (useW && W.IsReady())
+                foreach (
+                    var hero in
+                        ObjectManager.Get<Obj_AI_Hero>()
+                            .Where(hero => hero.IsValidTarget(Orbwalking.GetRealAutoAttackRange(hero) + W.Range)))
+                    W.CastOnUnit(ObjectManager.Player);
+
+            if (useQ && Q.IsReady())
+            {
+                var t = SimpleTs.GetTarget(Q.Range, SimpleTs.DamageType.Magical);
+                if (t != null)
+                    if (Q.Cast(t) == Spell.CastStates.SuccessfullyCasted)
+                        return;
+            }
+
+            if (useE && E.IsReady())
+            {
+                var t = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Magical);
+                if (t != null)
+                    if (E.Cast(t, false, true) == Spell.CastStates.SuccessfullyCasted)
+                        return;
+            }
+
+            if (useR && R.IsReady() && UltimateBuffStacks < rLim)
+            {
+                var t = SimpleTs.GetTarget(R.Range, SimpleTs.DamageType.Magical);
+                if (t != null)
+                    R.Cast(t, false, true);
+            }
+        }
+
+        public override void Orbwalking_AfterAttack(Obj_AI_Base unit, Obj_AI_Base target)
+        {
+            if ((!ComboActive && !HarassActive) || !unit.IsMe || (!(target is Obj_AI_Hero))) return;
+
+            var useQ = GetValue<bool>("UseQ" + (ComboActive ? "C" : "H"));
+            var useW = GetValue<bool>("UseW" + (ComboActive ? "C" : "H"));
+            var useE = GetValue<bool>("UseE" + (ComboActive ? "C" : "H"));
+            var useR = GetValue<bool>("UseR" + (ComboActive ? "C" : "H"));
+            var rLim = GetValue<Slider>("Rlim" + (ComboActive ? "C" : "H")).Value;
+
+            if (useW && W.IsReady())
+                W.CastOnUnit(ObjectManager.Player);
+
+            if (useQ && Q.IsReady())
+                if (Q.Cast(target) == Spell.CastStates.SuccessfullyCasted)
+                    return;
+
+            if (useE && E.IsReady())
+                if (E.Cast(target, false, true) == Spell.CastStates.SuccessfullyCasted)
+                    return;
+
+            if (useR && R.IsReady() && UltimateBuffStacks < rLim)
+                R.Cast(target, false, true);
+        }
+
+        private static int GetUltimateBuffStacks()
+        {
+            return (from buff in ObjectManager.Player.Buffs
+                where buff.DisplayName.ToLower() == "kogmawlivingartillery"
+                select buff.Count).FirstOrDefault();
+        }
+
+        public override bool ComboMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("UseQC" + Id, "浣跨敤 Q").SetValue(true));
+            config.AddItem(new MenuItem("UseWC" + Id, "浣跨敤 W").SetValue(true));
+            config.AddItem(new MenuItem("UseEC" + Id, "浣跨敤 E").SetValue(true));
+            config.AddItem(new MenuItem("UseRC" + Id, "浣跨敤 R").SetValue(true));
+            config.AddItem(new MenuItem("RlimC" + Id, "浣跨敤R闄愬埗浜烘暟").SetValue(new Slider(3, 5, 1)));
+            return true;
+        }
+
+        public override bool HarassMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("UseQH" + Id, "浣跨敤 Q").SetValue(false));
+            config.AddItem(new MenuItem("UseWH" + Id, "浣跨敤 W").SetValue(false));
+            config.AddItem(new MenuItem("UseEH" + Id, "浣跨敤 E").SetValue(false));
+            config.AddItem(new MenuItem("UseRH" + Id, "浣跨敤 R").SetValue(true));
+            config.AddItem(new MenuItem("RlimH" + Id, "浣跨敤R闄愬埗浜烘暟").SetValue(new Slider(1, 5, 1)));
+            return true;
+        }
+
+        public override bool DrawingMenu(Menu config)
+        {
+            config.AddItem(
+                new MenuItem("DrawQ" + Id, "Q 鑼冨洿").SetValue(new Circle(true,
+                    System.Drawing.Color.FromArgb(100, 255, 0, 255))));
+            config.AddItem(
+                new MenuItem("DrawW" + Id, "W 鑼冨洿").SetValue(new Circle(true,
+                    System.Drawing.Color.FromArgb(100, 255, 0, 255))));
+            config.AddItem(
+                new MenuItem("DrawE" + Id, "E 鑼冨洿").SetValue(new Circle(false,
+                    System.Drawing.Color.FromArgb(100, 255, 0, 255))));
+            config.AddItem(
+                new MenuItem("DrawR" + Id, "R 鑼冨洿").SetValue(new Circle(false,
+                    System.Drawing.Color.FromArgb(100, 255, 0, 255))));
+            return true;
+        }
+
+        public override bool MiscMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("UseRM" + Id, "浣跨敤R鎶汉澶磡").SetValue(true));
+            return true;
+        }
+
+        public override bool ExtrasMenu(Menu config)
+        {
+
+            return true;
+        }
+
+    }
+}
---
+++ Marksman ADC合集（全英雄汉化） by Esk0r/Twitch.cs
@@ -0,0 +1,119 @@
+#region
+
+using System;
+using System.Drawing;
+using System.Linq;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace Marksman
+{
+    internal class Twitch : Champion
+    {
+        public Spell W;
+        public Spell E;
+
+        public Twitch()
+        {
+            Utils.PrintMessage("Twitch loaded 姹夊寲by浜岀嫍!QQ缇361630847.");
+
+            W = new Spell(SpellSlot.W, 950);
+            W.SetSkillshot(0.25f, 120f, 1400f, false, SkillshotType.SkillshotCircle);
+            E = new Spell(SpellSlot.E, 1200);
+        }
+
+        public override void Orbwalking_AfterAttack(Obj_AI_Base unit, Obj_AI_Base target)
+        {
+            if ((!ComboActive && !HarassActive) || !unit.IsMe || (!(target is Obj_AI_Hero))) return;
+
+            var useW = GetValue<bool>("UseW" + (ComboActive ? "C" : "H"));
+
+            if (useW && W.IsReady())
+                W.Cast(target, false, true);
+        }
+
+        public override void Drawing_OnDraw(EventArgs args)
+        {
+            Spell[] spellList = { W };
+            foreach (var spell in spellList)
+            {
+                var menuItem = GetValue<Circle>("Draw" + spell.Slot);
+                if (menuItem.Active)
+                    Utility.DrawCircle(ObjectManager.Player.Position, spell.Range, menuItem.Color);
+            }
+        }
+
+        public override void Game_OnGameUpdate(EventArgs args)
+        {
+            if (Orbwalking.CanMove(100) && (ComboActive || HarassActive))
+            {
+                var useW = GetValue<bool>("UseW" + (ComboActive ? "C" : "H"));
+                var useE = GetValue<bool>("UseE" + (ComboActive ? "C" : "H"));
+
+                if (useW)
+                {
+                    var wTarget = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Physical);
+                    if (W.IsReady() && wTarget.IsValidTarget())
+                        W.Cast(wTarget, false, true);
+                }
+
+                if (useE && E.IsReady())
+                {
+                    var eTarget = SimpleTs.GetTarget(E.Range, SimpleTs.DamageType.Physical);
+                    if (eTarget.IsValidTarget(E.Range))
+                    {
+                        foreach (var buff in eTarget.Buffs.Where(buff => buff.DisplayName.ToLower() == "twitchdeadlyvenom").Where(buff => buff.Count == 6))
+                        {
+                            E.Cast();
+                        }
+                    }
+                    
+                }
+            }
+
+            if (GetValue<bool>("UseEM") && E.IsReady())
+            {
+                foreach (var hero in ObjectManager.Get<Obj_AI_Hero>().Where(hero => hero.IsValidTarget(E.Range) && (ObjectManager.Player.GetSpellDamage(hero, SpellSlot.E) - 10 > hero.Health)))
+                {
+                    E.Cast();
+                }
+            }
+        }
+
+        public override bool ComboMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("UseWC" + Id, "浣跨敤 W").SetValue(true));
+            config.AddItem(new MenuItem("UseEC" + Id, "鑷姩E(鏈€澶у眰鏁?").SetValue(true));
+            return true;
+        }
+
+        public override bool HarassMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("UseWH" + Id, "浣跨敤 W").SetValue(false));
+            config.AddItem(new MenuItem("UseEH" + Id, "鑷姩E(鏈€澶у眰鏁?").SetValue(false));
+            return true;
+        }
+
+        public override bool DrawingMenu(Menu config)
+        {
+            config.AddItem(
+                new MenuItem("DrawW" + Id, "W 鑼冨洿").SetValue(new Circle(true, Color.FromArgb(100, 255, 0, 255))));
+            return true;
+        }
+
+        public override bool MiscMenu(Menu config)
+        {
+            config.AddItem(new MenuItem("UseEM" + Id, "浣跨敤E鎶汉澶磡").SetValue(true));
+            return true;
+        }
+
+        public override bool ExtrasMenu(Menu config)
+        {
+
+            return true;
+        }
+
+    }
+}
