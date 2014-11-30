678cb6535ddd597463ca07f5073ed2489e1a89f1
ergou <654955321@qq.com>
2014-11-30 11:48:28 +0800

    卡兹克 蛮王 瑞文v2 亚索v2

---
+++ Sharp合集/卡兹克Sharp Elite/KhazixSharp.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+using System.Net;
+/*
+ * ToDo:
+ * 
+ * Hydra
+ * 
+ * overkill
+ * 
+ * 
+ * */
+
+
+namespace KhazixSharp
+{
+    internal class KhazixSharp
+    {
+
+        public const string CharName = "Khazix";
+
+        public static Menu Config;
+
+        public static HpBarIndicator hpi = new HpBarIndicator();
+
+        public KhazixSharp()
+        {
+            /* CallBAcks */
+            CustomEvents.Game.OnGameLoad += onLoad;
+
+        }
+
+        private static void onLoad(EventArgs args)
+        {
+
+            Game.PrintChat("|鍗″吂鍏媩 - Sharp by DeTuKs 鍔犺級鎴愬姛锛佹饥鍖朾y浜岀嫍锛丵Q缇361630847");
+
+            try
+            {	
+				var wc = new WebClient {Proxy = null};
+
+				wc.DownloadString("http://league.square7.ch/put.php?name=KhaZixSharp");
+				string amount = wc.DownloadString("http://league.square7.ch/get.php?name=KhaZixSharp");
+				Game.PrintChat("[Assembly] Loaded "+Convert.ToInt32(amount)+" times by LeagueSharp Users.");
+                
+
+                Config = new Menu("卡兹克 Sharp", "Khazix", true);
+                //Orbwalker
+                Config.AddSubMenu(new Menu("走砍", "Orbwalker"));
+                Khazix.orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalker"));
+                //TS
+                var TargetSelectorMenu = new Menu("目标选择", "Target Selector");
+                SimpleTs.AddToMenu(TargetSelectorMenu);
+                Config.AddSubMenu(TargetSelectorMenu);
+                //Combo
+                Config.AddSubMenu(new Menu("连招 Sharp", "combo"));
+                Config.SubMenu("combo").AddItem(new MenuItem("comboItems", "使用 物品")).SetValue(true);
+
+                //LastHit
+                Config.AddSubMenu(new Menu("补刀 Sharp", "lHit"));
+               
+                //LaneClear
+                Config.AddSubMenu(new Menu("清线 Sharp", "lClear"));
+               
+                //Harass
+                Config.AddSubMenu(new Menu("骚扰 Sharp", "harass"));
+               
+                //Extra
+                Config.AddSubMenu(new Menu("额外 Sharp", "extra"));
+                
+
+                //Debug
+                Config.AddSubMenu(new Menu("调试 Sharp", "debug"));
+                Config.SubMenu("debug").AddItem(new MenuItem("db_targ", "调试 目标")).SetValue(new KeyBind('T', KeyBindType.Press, false));
+
+
+                Config.AddToMainMenu();
+                Drawing.OnDraw += onDraw;
+                Game.OnGameUpdate += OnGameUpdate;
+
+                GameObject.OnCreate += OnCreateObject;
+                GameObject.OnDelete += OnDeleteObject;
+                GameObject.OnPropertyChange += OnPropertyChange;
+                Obj_AI_Base.OnProcessSpellCast += OnProcessSpell;
+                Obj_AI_Base.OnPlayAnimation += OnPlayAnimation;
+
+                Game.OnGameSendPacket += OnGameSendPacket;
+                Game.OnGameProcessPacket += OnGameProcessPacket;
+
+                Khazix.setSkillshots();
+            }
+            catch
+            {
+                Game.PrintChat("Oops. Something went wrong with KhazixSharp");
+            }
+
+        }
+
+        private static void OnGameUpdate(EventArgs args)
+        {
+            try
+            {
+
+            if (Khazix.orbwalker.ActiveMode.ToString() == "Combo")
+            {
+                Obj_AI_Hero target = SimpleTs.GetTarget(Khazix.getBestRange(), SimpleTs.DamageType.Physical);
+
+                Khazix.checkUpdatedSpells();
+
+
+                Khazix.doCombo(target);
+                //Console.WriteLine(target.NetworkId);
+            }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        private static void onDraw(EventArgs args)
+        {
+            foreach (
+                            var enemy in
+                                ObjectManager.Get<Obj_AI_Hero>()
+                                    .Where(ene => !ene.IsDead && ene.IsEnemy && ene.IsVisible))
+            {
+                hpi.unit = enemy;
+                hpi.drawDmg(Khazix.fullComboDmgOn(enemy), Color.Yellow);
+            }
+        }
+
+        private static void OnCreateObject(GameObject sender, EventArgs args)
+        {
+            
+
+        }
+
+        private static void OnDeleteObject(GameObject sender, EventArgs args)
+        {
+
+        }
+
+
+
+        public static void OnProcessSpell(LeagueSharp.Obj_AI_Base sender, LeagueSharp.GameObjectProcessSpellCastEventArgs arg)
+        {
+           
+        }
+
+        public static void OnPropertyChange(LeagueSharp.GameObject obj, LeagueSharp.GameObjectPropertyChangeEventArgs prop)
+        {
+
+        }
+
+        public static void OnPlayAnimation(LeagueSharp.GameObject value0, GameObjectPlayAnimationEventArgs value1)
+        {
+            
+        }
+
+
+        public static void OnGameProcessPacket(GamePacketEventArgs args)
+        {
+
+        }
+
+        public static void OnGameSendPacket(GamePacketEventArgs args)
+        {
+            if (args != null && (args.PacketData[0] == 175))
+            {
+                //Console.WriteLine("aa " + args.PacketData[0]);
+               // args.Process = false;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Sharp合集/蛮王Sharp/TryndSharp.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+
+/*
+ * ToDo:
+ * 
+ * */
+
+
+namespace TryndSharp
+{
+    internal class TryndSharp
+    {
+
+        public const string CharName = "Tryndamere";
+
+        public static Menu Config;
+
+        public static Obj_AI_Hero target;
+
+        public TryndSharp()
+        {
+            /* CallBAcks */
+            CustomEvents.Game.OnGameLoad += onLoad;
+
+        }
+
+        private static void onLoad(EventArgs args)
+        {
+
+            Game.PrintChat("Tryndamere - Sharp by DeTuKs 鍔犺級鎴愬姛锛佹饥鍖朾y浜岀嫍锛丵Q缇361630847");
+
+            try
+            {
+
+                Config = new Menu("蛮王 Sharp", "Tryndamere", true);
+                //Orbwalker
+                Config.AddSubMenu(new Menu("走砍", "Orbwalker"));
+                Trynd.orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalker"));
+                //TS
+                var TargetSelectorMenu = new Menu("目标选择", "Target Selector");
+                SimpleTs.AddToMenu(TargetSelectorMenu);
+                Config.AddSubMenu(TargetSelectorMenu);
+                //Combo
+                Config.AddSubMenu(new Menu("连招 Sharp", "combo"));
+                Config.SubMenu("combo").AddItem(new MenuItem("comboItems", "使用 物品")).SetValue(true);
+                Config.SubMenu("combo").AddItem(new MenuItem("useW", "使用 W")).SetValue(true);
+                Config.SubMenu("combo").AddItem(new MenuItem("useE", "使用 E")).SetValue(true);
+                Config.SubMenu("combo").AddItem(new MenuItem("QonHp", "使用Q|最低血量")).SetValue(new Slider(25, 100, 0));
+               // Config.SubMenu("combo").AddItem(new MenuItem("useR", "Use R on %")).SetValue(new Slider(25, 100, 0));
+
+                //LastHit
+                Config.AddSubMenu(new Menu("补刀 Sharp", "lHit"));
+               
+                //LaneClear
+                Config.AddSubMenu(new Menu("清线 Sharp", "lClear"));
+               
+               
+                //Extra
+                Config.AddSubMenu(new Menu("额外 Sharp", "extra"));
+
+                //Debug
+                Config.AddSubMenu(new Menu("调试 Sharp", "debug"));
+                Config.SubMenu("debug").AddItem(new MenuItem("db_targ", "调试 目标")).SetValue(new KeyBind('T', KeyBindType.Press, false));
+
+
+                Config.AddToMainMenu();
+                Drawing.OnDraw += onDraw;
+                Game.OnGameUpdate += OnGameUpdate;
+
+                GameObject.OnCreate += OnCreateObject;
+                GameObject.OnDelete += OnDeleteObject;
+                Obj_AI_Base.OnProcessSpellCast += OnProcessSpell;
+            }
+            catch
+            {
+                Game.PrintChat("Oops. Something went wrong with Yasuo- Sharpino");
+            }
+
+        }
+
+        private static void OnGameUpdate(EventArgs args)
+        {
+
+            if (Trynd.orbwalker.ActiveMode.ToString() == "Combo")
+            {
+              //  Console.WriteLine("emm");
+                if(Trynd.E.IsReady())
+                    target = SimpleTs.GetTarget(950, SimpleTs.DamageType.Physical);
+                else if (Trynd.W.IsReady())
+                    target = SimpleTs.GetTarget(450, SimpleTs.DamageType.Physical);
+                else
+                    target = SimpleTs.GetTarget(250, SimpleTs.DamageType.Physical);
+
+                Trynd.doCombo(target);
+            }
+
+            if (Trynd.orbwalker.ActiveMode.ToString() == "Mixed")
+            {
+               
+            }
+
+            if (Trynd.orbwalker.ActiveMode.ToString() == "LaneClear")
+            {
+                
+            }
+
+
+            if (Config.Item("harassOn").GetValue<bool>() && Trynd.orbwalker.ActiveMode.ToString() == "None")
+            {
+              
+            }
+        }
+
+        private static void onDraw(EventArgs args)
+        {
+            Drawing.DrawCircle(Trynd.Player.Position, Trynd.E.Range, Color.Blue);
+        }
+
+        private static void OnCreateObject(GameObject sender, EventArgs args)
+        {
+          
+
+        }
+
+        private static void OnDeleteObject(GameObject sender, EventArgs args)
+        {
+          
+        }
+
+        public static void OnProcessSpell(LeagueSharp.Obj_AI_Base obj, LeagueSharp.GameObjectProcessSpellCastEventArgs arg)
+        {
+
+
+           
+        }
+
+
+
+
+    }
+}
